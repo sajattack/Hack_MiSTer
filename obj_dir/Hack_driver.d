4a9f6a36dd3aee08384ef8b87dd483e9cc827928
Paul Sajna <sajattack@gmail.com>
2019-12-03 00:50:22 -0800

    remove obj_dir

--- obj_dir/Hack_driver.d
+++
@@ -1,5 +0,0 @@
-Hack_driver.o: ../sim/Hack_driver.cpp ../sim/../obj_dir/Vverilator_top.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h
--- obj_dir/Vverilator_top__ALLcls.d
+++
@@ -1,5 +0,0 @@
-Vverilator_top__ALLcls.o: Vverilator_top__ALLcls.cpp Vverilator_top.cpp \
- Vverilator_top.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h Vverilator_top__Syms.h \
- Vverilator_top_h_Mux.h Vverilator_top_FullAdder.h \
- Vverilator_top_h_Mux.cpp Vverilator_top_FullAdder.cpp
